90fc2bdd9b5140792740a769305aa3c014f7b38a
Alex Mavrogiannis <nalfemp@gmail.com>
2015-10-17 21:49:49 -0700

    sync

--- experiment.py
+++ experiment.py
@@ -1,6 +1,8 @@
 import docker
 from subprocess import call
 import os
+import signal
+import sys
 
 cwd = os.getcwd()
 
@@ -8,11 +10,33 @@ teams = int(raw_input("How many teams of 4 are there in the experiment? "))
 
 c = docker.Client()
 
+# cleanup
+os.system("sudo rm -rf team*")
+
+mysql_conts = []
+
+def sigint_handler(signal, frame):
+    for ctr in mysql_conts:
+        c.remove_container(ctr.get("Id"))
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint_handler)
+
 for team in range(teams):
     teamid = "team"+str(team)
-    call(["cp", "-rf", "template", teamid])
-
     print teamid
+
+    call(["cp", "-rf", "template", teamid])
+    os.system("cd " + teamid + "&& git branch test && git checkout test")
+
+    # Launch MySQL container
+    mysqlpath = teamid+"mysql"
+    ctr = c.create_container(image="mysql/mysql-server", 
+                             environment={"MYSQL_ROOT_PASSWORD": "root"},
+                             name=mysqlpath)
+    resp = c.start(container=ctr.get("Id"))
+    mysql_conts.append(ctr)
+
     
     for member in range(4):
         ctr = c.create_container(image="afein/vt-experiment", 
@@ -21,7 +45,8 @@ for team in range(teams):
                                  host_config=c.create_host_config(port_bindings={
                                      22: ('127.0.0.1',),
                                      6080: ('127.0.0.1',)
-                                 }, binds = [cwd + "/" + teamid + ":/opt/remotegit"]
+                                 }, binds = [cwd + "/" + teamid + ":/opt/remotegit"],
+                                 links={mysqlpath:"mysql"}
                                  ))
         resp = c.start(container=ctr.get("Id"))
 
